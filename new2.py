b1b9a893dd5890a75c3372c1c89a75de4cb222d0
kevinsukesh <kiwicodes93@gmail.com>
2025-05-10 21:17:40 +0400

    2nd commit when I created new3.py

--- new2.py
+++ new2.py
@@ -1,1 +1,7 @@
-print("Hello, Kevin!")
+course = "  python programming"
+course = course + " dragon"
+print(course.strip())
+if ("dragon" not in course):
+    print("No dragons here!")
+else:
+    print("Dragons are here!") 
