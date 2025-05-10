98f9699a7ecfaaacf658a9dfa46313cfb533fb89
kevinsukesh <kiwicodes93@gmail.com>
2025-05-10 13:59:23 +0400

    this is the initial commit adding the thing is did so far; this is a local commit.

---
+++ new.py
@@ -0,0 +1,10 @@
+# trying out formated strings
+
+first_name = "Kevin"
+last_name = "Smith"
+
+print(f"\nHello, {first_name} {last_name}!")
+print(f"\nMy first name is {first_name} and it has {len(first_name)} characters.\n")
+print(f"My last name starts with the letter \"{last_name[0]}\".\n")
+
+# trying out formated strings
